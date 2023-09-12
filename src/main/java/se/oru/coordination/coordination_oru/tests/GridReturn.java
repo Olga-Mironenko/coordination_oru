1d7f06bbf7e980cdd802f2689b8d1f5216d79fd6
Olga-Mironenko <124531645+Olga-Mironenko@users.noreply.github.com>
2023-09-12 09:40:26 +0200

    Added GridReturn. Added `CriticalSection.robotIDInferior` and corresponding stringification.

---
+++ src/main/java/se/oru/coordination/coordination_oru/tests/GridReturn.java
@@ -0,0 +1,187 @@
+package se.oru.coordination.coordination_oru.tests;
+
+import java.awt.Color;
+
+import com.vividsolutions.jts.geom.Coordinate;
+import org.metacsp.multi.spatioTemporal.paths.Pose;
+
+import se.oru.coordination.coordination_oru.code.*;
+import se.oru.coordination.coordination_oru.motionplanning.ompl.ReedsSheppCarPlanner;
+import se.oru.coordination.coordination_oru.simulation2D.EmergencyBreaker;
+import se.oru.coordination.coordination_oru.simulation2D.TrajectoryEnvelopeCoordinatorSimulation;
+import se.oru.coordination.coordination_oru.simulation2D.TrajectoryEnvelopeTrackerRK4;
+import se.oru.coordination.coordination_oru.util.*;
+import se.oru.coordination.coordination_oru.util.gates.GatedThread;
+
+import static se.oru.coordination.coordination_oru.util.Printer.print;
+
+public class GridReturn {
+    enum Scenario {
+        BASIC
+    }
+
+    public static void main(String[] args) {
+        Printer.resetTime();
+        print("started");
+
+        BrowserVisualization.isStatusText = true;
+        GatedThread.enable();
+
+        new GatedThread("startScenario") {
+            @Override
+            public void runCore() {
+                try {
+                    startScenario();
+                } catch (NoPathFound e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }.start();
+
+        GatedThread.runGatekeeper();
+    }
+
+    protected static void startScenario() throws NoPathFound {
+        final String scenarioString = System.getenv().get("SCENARIO");
+        final Scenario scenario = Scenario.BASIC;
+
+        AbstractVehicle.scenarioId = String.valueOf(scenario);
+
+        final double loopMinutes = 60;
+        final long loopTime = System.currentTimeMillis() + Math.round(loopMinutes * 60 * 1000);
+
+        final String YAML_FILE = "maps/map-grid.yaml";
+
+        final double xLeft = 4.0;
+        final double xRight = 56.0;
+        final double yTop = 56.5;
+        final double yBottom = 3.0;
+
+        final double xColumn1 = 14.5;
+        final double xColumn2 = 30.0;
+        final double xColumn3 = 45.7;
+        final double yRow1 = 44.0;
+        final double yRow2 = 30.0;
+        final double yRow3 = 15.5;
+
+        final double thetaDown = -Math.PI/2;
+        final double thetaUp = Math.PI/2;
+        final double thetaRight = 0;
+        final double thetaLeft = Math.PI;
+
+        final Pose column1Top = new Pose(xColumn1, yTop, thetaDown);
+        final Pose column2Top = new Pose(xColumn2, yTop, thetaDown);
+        final Pose column3Top = new Pose(xColumn3, yTop, thetaDown);
+
+        final Pose column1Bottom = new Pose(xColumn1, yBottom, thetaUp);
+        final Pose column2Bottom = new Pose(xColumn2, yBottom, thetaLeft); // left is for the robot 0 to look down
+        final Pose column3Bottom = new Pose(xColumn3, yBottom, thetaUp);
+
+        final Pose row1Left = new Pose(xLeft, yRow1, thetaRight);
+        final Pose row2Left = new Pose(xLeft, yRow2, thetaRight);
+        final Pose row3Left = new Pose(xLeft, yRow3, thetaRight);
+
+        final Pose row1Right = new Pose(xRight, yRow1, thetaLeft);
+        final Pose row2Right = new Pose(xRight, yRow2, thetaLeft);
+        final Pose row3Right = new Pose(xRight, yRow3, thetaLeft);
+
+        final Pose centerDownward = new Pose(xColumn2, yRow2, thetaDown);
+
+        final Pose humStart = column2Top;
+        final Pose humFinish = column2Bottom;
+
+        final boolean ishumLoop = true;
+
+        final Pose aut1Start = row1Left;
+        final Pose aut1Finish = row1Right;
+
+        final Pose aut2Start = row2Left;
+        final Pose aut2Finish = row2Right;
+
+        final Pose aut3Start = row3Left;
+        final Pose aut3Finish = row3Right;
+
+        final Pose aut4Start = column1Top;
+        final Pose aut4Finish = column1Bottom;
+
+        final Pose aut5Start = row3Left;
+        final Pose aut5Finish = row1Right;
+
+        final double precisionCoefficient = 1;
+        final double maxVelocity = 5.0 * precisionCoefficient;
+        final double maxAcceleration = 2.0 * precisionCoefficient;
+        final int trackingPeriod = (int) Math.round(100 / precisionCoefficient);
+
+        double xLength = 2.5;
+        double yLength = 1.5;
+        double xLengthInner = 1.5;
+        double yLengthInner = 1.0;
+
+        HumanControl.targetVelocityHumanInitial = maxVelocity;
+        HumanControl.targetVelocityHuman = maxVelocity;
+
+        AutonomousVehicle.planningAlgorithm = ReedsSheppCarPlanner.PLANNING_ALGORITHM.RRTConnect; // default
+        //AutonomousVehicle.planningAlgorithm = ReedsSheppCarPlanner.PLANNING_ALGORITHM.PRMstar; // too slow
+        //AutonomousVehicle.planningAlgorithm = ReedsSheppCarPlanner.PLANNING_ALGORITHM.SPARS; // too slow
+
+        AutonomousVehicle aut1 = null;
+        AutonomousVehicle aut2 = null;
+        AutonomousVehicle aut3 = null;
+        AutonomousVehicle aut4 = null;
+        AutonomousVehicle aut5 = null;
+
+        // TODO: `maxAcceleration` passed here is not used by `tec`.
+        AutonomousVehicle hum0 = new HumanDrivenVehicle(0, Color.GREEN, Color.BLUE, maxVelocity, maxAcceleration, xLength, yLength);
+        aut1 = new AutonomousVehicle(1, 0, Color.YELLOW, Color.YELLOW, maxVelocity, maxAcceleration, xLength, yLength);
+        aut2 = new AutonomousVehicle(2, 0, Color.YELLOW, Color.YELLOW, maxVelocity, maxAcceleration, xLength, yLength);
+        aut3 = new AutonomousVehicle(3, 0, Color.YELLOW, Color.YELLOW, maxVelocity, maxAcceleration, xLength, yLength);
+        //aut4 = new AutonomousVehicle(4, 0, Color.YELLOW, Color.YELLOW, maxVelocity, maxAcceleration, xLength, yLength);
+        //aut5 = new AutonomousVehicle(5, 0, Color.YELLOW, Color.YELLOW, maxVelocity, maxAcceleration, xLength, yLength);
+
+        TrajectoryEnvelopeTrackerRK4.emergencyBreaker = new EmergencyBreaker(false, false);
+
+        TrajectoryEnvelopeCoordinatorSimulation tec = new TrajectoryEnvelopeCoordinatorSimulation(2000, 1000, maxVelocity, maxAcceleration, trackingPeriod);
+        tec.setupSolver(0, 100000000);
+        tec.startInference();
+
+        Coordinate[] innerFootprint = AbstractVehicle.makeFootprint(xLengthInner, yLengthInner);
+        for (AbstractVehicle vehicle : new AbstractVehicle[] { hum0, aut1, aut2, aut3, aut4, aut5 }) {
+            if (vehicle != null) {
+                vehicle.innerFootprint = innerFootprint;
+                tec.setFootprint(vehicle.getID(), vehicle.getFootprint());
+                tec.setInnerFootprint(vehicle.getID(), vehicle.innerFootprint);
+            }
+        }
+
+        tec.placeRobot(hum0.getID(), humStart);
+        if (aut1 != null) tec.placeRobot(aut1.getID(), aut1Start);
+        if (aut2 != null) tec.placeRobot(aut2.getID(), aut2Start);
+        if (aut3 != null) tec.placeRobot(aut3.getID(), aut3Start);
+        if (aut4 != null) tec.placeRobot(aut4.getID(), aut4Start);
+        if (aut5 != null) tec.placeRobot(aut5.getID(), aut5Start);
+
+        Heuristics heuristics = new Heuristics();
+        tec.addComparator(heuristics.highestIDNumber());
+
+        tec.setUseInternalCriticalPoints(false);
+        tec.setYieldIfParking(true);
+        tec.setBreakDeadlocks(true, false, false);
+
+        var viz = new BrowserVisualization();
+        viz.setMap(YAML_FILE);
+        viz.setInitialTransform(7.0, 5.0, 5.0);
+        tec.setVisualization(viz);
+
+        Missions.setMap(YAML_FILE);
+        Missions.startMissionDispatchers(tec, loopTime);
+        Missions.loopMissions.put(hum0.getID(), ishumLoop);
+
+        final boolean isInverse = true;
+        Missions.enqueueMissions(hum0, humStart, humFinish, isInverse);
+        if (aut1 != null) Missions.enqueueMissions(aut1, aut1Start, aut1Finish, isInverse);
+        if (aut2 != null) Missions.enqueueMissions(aut2, aut2Start, aut2Finish, isInverse);
+        if (aut3 != null) Missions.enqueueMissions(aut3, aut3Start, aut3Finish, isInverse);
+        if (aut4 != null) Missions.enqueueMissions(aut4, aut4Start, aut4Finish, isInverse);
+        if (aut5 != null) Missions.enqueueMissions(aut5, aut5Start, aut5Finish, isInverse);
+    }
+}
