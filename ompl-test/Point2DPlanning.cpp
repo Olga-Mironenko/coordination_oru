1a5112b2fa90b9094300b22d1d04c4b461e05cbb
Olga-Mironenko <124531645+Olga-Mironenko@users.noreply.github.com>
2024-09-14 17:35:11 +0200

    ompl-test/Point2DPlanning.cpp: extracted the PRM roadmap graph (for further re-use)

--- ompl-test/Point2DPlanning.cpp
+++ ompl-test/Point2DPlanning.cpp
@@ -1,139 +1,164 @@
-/*********************************************************************
- * Software License Agreement (BSD License)
- *
- *  Copyright (c) 2013, Rice University
- *  All rights reserved.
- *
- *  Redistribution and use in source and binary forms, with or without
- *  modification, are permitted provided that the following conditions
- *  are met:
- *
- *   * Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *   * Redistributions in binary form must reproduce the above
- *     copyright notice, this list of conditions and the following
- *     disclaimer in the documentation and/or other materials provided
- *     with the distribution.
- *   * Neither the name of the Rice University nor the names of its
- *     contributors may be used to endorse or promote products derived
- *     from this software without specific prior written permission.
- *
- *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
- *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- *  POSSIBILITY OF SUCH DAMAGE.
- *********************************************************************/
+/* Based on the demo from OMPL. Its author: Ioan Sucan */
 
-/* Author: Ioan Sucan */
+#include <iostream>
 
+#include <boost/graph/graphml.hpp>
+#include <boost/filesystem.hpp>
+
+#include <ompl/base/PlannerDataGraph.h>
+#include <ompl/base/samplers/DeterministicStateSampler.h>
+#include <ompl/base/samplers/deterministic/HaltonSequence.h>
 #include <ompl/base/spaces/RealVectorStateSpace.h>
 #include <ompl/geometric/SimpleSetup.h>
 #include <ompl/geometric/planners/rrt/RRTstar.h>
 #include <ompl/geometric/planners/rrt/RRTConnect.h>
 #include <ompl/geometric/planners/prm/PRMstar.h>
-
 #include <ompl/util/PPM.h>
-#include <ompl/base/samplers/DeterministicStateSampler.h>
-#include <ompl/base/samplers/deterministic/HaltonSequence.h>
 #include <ompl/config.h>
-
-#include <boost/filesystem.hpp>
-#include <iostream>
 
 namespace ob = ompl::base;
 namespace og = ompl::geometric;
 
+std::vector<std::pair<double, double> > plannerDataToPoints(const ob::PlannerData& pd)
+{
+    std::ostringstream streamGraphml;
+    pd.printGraphML(streamGraphml);
+    // std::cout << streamGraphml.str() << std::endl;
+
+    std::string s = streamGraphml.str();
+    const std::string prefix = "<data key=\"key0\">";
+    const std::string postfix = "</data>";
+
+    std::vector<std::pair<double, double> > points;
+
+    size_t last = 0;
+    size_t next = 0;
+    while ((next = s.find('\n', last)) != std::string::npos) {
+        std::string line = s.substr(last, next - last);
+        const size_t indexPrefix = line.find(prefix);
+        if (indexPrefix != std::string::npos) {
+            const size_t indexPostfix = line.find(postfix);
+            assert(indexPostfix != std::string::npos);
+            std::string pair = line.substr(indexPrefix + prefix.size(), indexPostfix - indexPrefix - prefix.size());
+
+            const size_t indexComma = pair.find(',');
+            assert(indexComma != std::string::npos);
+            double x = strtod(pair.substr(0, indexComma).c_str(), nullptr);
+            double y = strtod(pair.substr(indexComma + 1).c_str(), nullptr);
+
+            // std::cout << x << ", " << y << std::endl;
+
+            points.push_back(std::make_pair(x, y));
+        }
+        last = next + 1;
+    }
+
+    return points;
+}
+
 class Plane2DEnvironment
 {
 public:
-    Plane2DEnvironment(const char *ppm_file, bool use_deterministic_sampling = false)
+    explicit Plane2DEnvironment(const char *ppm_file)
     {
-        bool ok = false;
-        useDeterministicSampling_ = use_deterministic_sampling;
         try
         {
             ppm_.loadFile(ppm_file);
-            ok = true;
         }
         catch (ompl::Exception &ex)
         {
             OMPL_ERROR("Unable to load %s.\n%s", ppm_file, ex.what());
+            return;
         }
-        if (ok)
-        {
-            auto space(std::make_shared<ob::RealVectorStateSpace>());
-            space->addDimension(0.0, ppm_.getWidth());
-            space->addDimension(0.0, ppm_.getHeight());
-            maxWidth_ = ppm_.getWidth() - 1;
-            maxHeight_ = ppm_.getHeight() - 1;
-            ss_ = std::make_shared<og::SimpleSetup>(space);
 
-            // set state validity checking for this space
-            ss_->setStateValidityChecker([this](const ob::State *state) { return isStateValid(state); });
-            space->setup();
-            ss_->getSpaceInformation()->setStateValidityCheckingResolution(1.0 / space->getMaximumExtent());
+       // TODO: an IntegerStateSpace
+        auto space(std::make_shared<ob::RealVectorStateSpace>());
+        width_ = ppm_.getWidth();
+        height_ = ppm_.getHeight();
+        space->addDimension(0.0, width_);
+        space->addDimension(0.0, height_);
+        ss_ = std::make_shared<og::SimpleSetup>(space);
 
-            // set the deterministic sampler
-            // 2D space, no need to specify bases specifically
-            if (useDeterministicSampling_)
-            {
-                // PRMstar can use the deterministic sampling
-                ss_->setPlanner(std::make_shared<og::PRMstar>(ss_->getSpaceInformation()));
-                space->setStateSamplerAllocator(std::bind(&Plane2DEnvironment::allocateHaltonStateSamplerRealVector,
-                                                          this, std::placeholders::_1, 2,
-                                                          std::vector<unsigned int>{2, 3}));
-            }
-        }
+        // set state validity checking for this space
+        ss_->setStateValidityChecker([this](const ob::State *state) { return isStateValid(state); });
+        space->setup();
+        ss_->getSpaceInformation()->setStateValidityCheckingResolution(1.0 / space->getMaximumExtent());
+
+        // set the deterministic sampler
+        // 2D space, no need to specify bases specifically
+        // PRMstar can use the deterministic sampling
+        planner_ = std::make_shared<og::PRMstar>(ss_->getSpaceInformation());
+        ss_->setPlanner(planner_);
+        space->setStateSamplerAllocator(
+            std::bind(
+                &Plane2DEnvironment::allocateHaltonStateSamplerRealVector,
+                this, std::placeholders::_1, 2, std::vector<unsigned int>{2, 3}
+            ));
     }
 
-    bool plan(unsigned int start_row, unsigned int start_col, unsigned int goal_row, unsigned int goal_col)
+    bool plan(unsigned int xStart, unsigned int yStart, unsigned int xGoal, unsigned int yGoal)
     {
         if (!ss_)
             return false;
 
+        assert(xStart < width_);
+        assert(yStart < height_);
+
         ob::ScopedState<> start(ss_->getStateSpace());
-        start[0] = start_row;
-        start[1] = start_col;
+        start[0] = xStart;
+        start[1] = yStart;
 
         ob::ScopedState<> goal(ss_->getStateSpace());
-        goal[0] = goal_row;
-        goal[1] = goal_col;
+        goal[0] = xGoal;
+        goal[1] = yGoal;
 
         ss_->setStartAndGoalStates(start, goal);
+        const size_t numStarts = 1;
+        const size_t numGoals = 1;
 
         // generate a few solutions; all will be added to the goal;
+        OMPL_INFORM("%d vertices", planner_->getRoadmap().m_vertices.size());
         for (int i = 0; i < 1; ++i)
         {
-            if (ss_->getPlanner())
-                ss_->getPlanner()->clear();
-            ss_->solve();
+            // if (ss_->getPlanner())
+            //     ss_->getPlanner()->clearQuery();
+            ss_->solve(0.1);
+            OMPL_INFORM("%d vertices in the roadmap", planner_->getRoadmap().m_vertices.size());
         }
 
-        // ss_->getPlanner()->getPlannerData() // TODO
+        auto roadmap = planner_->getRoadmap();
+        int numPointsInRoadmap = 0;
+        for (int i = 0; i < roadmap.m_vertices.size(); ++i) {
+            auto vertex = roadmap.m_vertices[i];
+            size_t numEdges = vertex.m_out_edges.size();
+            bool isPoint = i < numStarts + numGoals || numEdges > 0;
+            if (isPoint) {
+                numPointsInRoadmap++;
+            }
+            auto state = vertex.m_property.m_value;
+            double x = state->as<ob::RealVectorStateSpace::StateType>()->values[0];
+            double y = state->as<ob::RealVectorStateSpace::StateType>()->values[1];
+            OMPL_INFORM("roadmap vertex %d: x=%.1f, y=%.1f, numEdges=%d, isPoint=%d", i, x, y, numEdges, isPoint ? 1 : 0);
+        }
+
+        ob::PlannerData pd(ss_->getSpaceInformation());
+        ss_->getPlannerData(pd);
+        points_ = plannerDataToPoints(pd);
+        OMPL_INFORM("%d points in the planner data", points_.size());
+        for (int i = 0; i < points_.size(); ++i) {
+            auto point = points_[i];
+            OMPL_INFORM("planner data point %d: x=%.1f, y=%.1f", i, point.first, point.second);
+        }
+        assert(points_.size() <= roadmap.m_vertices.size());
+        assert(points_.size() == numPointsInRoadmap);
 
         const std::size_t ns = ss_->getProblemDefinition()->getSolutionCount();
-        OMPL_INFORM("Found %d solutions", (int)ns);
+        OMPL_INFORM("Found %d solutions (haveSolutionPath=%d)", ns, ss_->haveSolutionPath() ? 1 : 0);
+        // TODO: `haveSolutionPath` seems to be always `true`
 
         if (ss_->haveSolutionPath())
         {
-            if (!useDeterministicSampling_)
-                ss_->simplifySolution();
-
             og::PathGeometric &p = ss_->getSolutionPath();
-            if (!useDeterministicSampling_)
-            {
-                ss_->getPathSimplifier()->simplifyMax(p);
-                ss_->getPathSimplifier()->smoothBSpline(p);
-            }
-
             return true;
         }
 
@@ -144,17 +169,45 @@ public:
     {
         if (!ss_ || !ss_->haveSolutionPath())
             return;
-        og::PathGeometric &p = ss_->getSolutionPath();
-        p.interpolate();
-        for (std::size_t i = 0; i < p.getStateCount(); ++i)
+        og::PathGeometric &path = ss_->getSolutionPath();
+
+        path.interpolate(); // TODO: why is it needed?
+
+        for (std::size_t i = 0; i < path.getStateCount(); ++i)
         {
-            const int w = std::min(maxWidth_, (int)p.getState(i)->as<ob::RealVectorStateSpace::StateType>()->values[0]);
-            const int h =
-                std::min(maxHeight_, (int)p.getState(i)->as<ob::RealVectorStateSpace::StateType>()->values[1]);
-            ompl::PPM::Color &c = ppm_.getPixel(h, w);
+            const auto state = path.getState(i)->as<ob::RealVectorStateSpace::StateType>();
+
+            const int x = static_cast<int>(state->values[0]);
+            assert(0 <= x && x < width_);
+
+            const int y = static_cast<int>(state->values[1]);
+            assert(0 <= y && y < height_);
+
+            ompl::PPM::Color &c = ppm_.getPixel(y, x);
             c.red = 255;
             c.green = 0;
             c.blue = 0;
+        }
+
+        for (const auto point : points_) {
+            const int x = static_cast<int>(point.first);
+            assert(0 <= x && x < width_);
+
+            const int y = static_cast<int>(point.second);
+            assert(0 <= y && y < height_);
+
+            for (int dx = -2; dx <= 2; ++dx) {
+                for (int dy = -2; dy <= 2; ++dy) {
+                    const int xp = x + dx;
+                    const int yp = y + dy;
+                    if (0 <= xp && xp < width_ && 0 <= yp && yp < height_) {
+                        ompl::PPM::Color &c = ppm_.getPixel(yp, xp);
+                        c.red = 0;
+                        c.green = 255;
+                        c.blue = 0;
+                    }
+                }
+            }
         }
     }
 
@@ -166,12 +219,23 @@ public:
     }
 
 private:
-    bool isStateValid(const ob::State *state) const
+    bool isStateValid(const ob::State *statePtr) const
     {
-        const int w = std::min((int)state->as<ob::RealVectorStateSpace::StateType>()->values[0], maxWidth_);
-        const int h = std::min((int)state->as<ob::RealVectorStateSpace::StateType>()->values[1], maxHeight_);
+        const auto state = statePtr->as<ob::RealVectorStateSpace::StateType>();
 
-        const ompl::PPM::Color &c = ppm_.getPixel(h, w);
+        const int x = static_cast<int>(state->values[0]);
+        assert(0 <= x && x <= width_);
+        if (x == width_) {
+            return false;
+        }
+
+        const int y = static_cast<int>(state->values[1]);
+        assert(0 <= y && y <= height_);
+        if (y == height_) {
+            return false;
+        }
+
+        const ompl::PPM::Color &c = ppm_.getPixel(y, x);
         return c.red > 127 && c.green > 127 && c.blue > 127;
     }
 
@@ -180,28 +244,28 @@ private:
     {
         // specify which deterministic sequence to use, here: HaltonSequence
         // optionally we can specify the bases used for generation (otherwise first dim prime numbers are used)
-        if (bases.size() != 0)
+        if (!bases.empty())
             return std::make_shared<ompl::base::RealVectorDeterministicStateSampler>(
                 space, std::make_shared<ompl::base::HaltonSequence>(bases.size(), bases));
-        else
-            return std::make_shared<ompl::base::RealVectorDeterministicStateSampler>(
-                space, std::make_shared<ompl::base::HaltonSequence>(dim));
+
+        return std::make_shared<ompl::base::RealVectorDeterministicStateSampler>(
+            space, std::make_shared<ompl::base::HaltonSequence>(dim));
     }
 
     og::SimpleSetupPtr ss_;
-    int maxWidth_;
-    int maxHeight_;
+    size_t width_;
+    size_t height_;
     ompl::PPM ppm_;
-    bool useDeterministicSampling_;
+    std::shared_ptr<og::PRMstar> planner_;
+    std::vector<std::pair<double, double> > points_;
 };
 
 int main(int /*argc*/, char ** /*argv*/)
 {
     std::cout << "OMPL version: " << OMPL_VERSION << std::endl;
 
-    boost::filesystem::path path(TEST_RESOURCES_DIR);
-    bool useDeterministicSampling = true;
-    Plane2DEnvironment env((path / "ppm/floor.ppm").string().c_str(), useDeterministicSampling);
+    const boost::filesystem::path path(TEST_RESOURCES_DIR);
+    Plane2DEnvironment env((path / "ppm/floor.ppm").string().c_str());
 
     if (env.plan(0, 0, 777, 1265))
     {
