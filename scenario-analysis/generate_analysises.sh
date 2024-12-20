bc04a16221c7ef17719325393b7ea0027de46984
Olga-Mironenko <124531645+Olga-Mironenko@users.noreply.github.com>
2024-12-20 16:37:40 +0100

    model.ipynb: generalizing code (new columns can be added easier now), making training faster, showing importance of features

--- scenario-analysis/generate_analysises.sh
+++ scenario-analysis/generate_analysises.sh
@@ -1,19 +1,31 @@
 #!/bin/bash
 
-set -eu -o pipefail
+set -eux -o pipefail
 
-[ $# -ge 0 ]
-if [ $# = 0 ]; then
-  indexes=({1..10})
-else
-  indexes=("$@")
-fi
+[ $# = 0 ]
+
+runnames=(
+  '20241203_170129_all600'
+  '20241213_104400_racing'
+  '20241214_122216_racing_passhum'
+)
+
+indexes=(
+#  {1..10}
+  1
+)
 
 fails=()
-for i_map in "${indexes[@]}"; do
-  if ! papermill -p I_MAP "$i_map" analysis.ipynb generated_analysis_map"$i_map".ipynb; then
-    fails+=("$i_map")
-  fi
+for runname in "${runnames[@]}"; do
+  for i_map in "${indexes[@]}"; do
+    opts=(
+      -p RUNNAME "$runname"
+      -p I_MAP "$i_map"
+    )
+    if ! papermill "${opts[@]}" analysis.ipynb generated_analysis_map"$i_map".ipynb; then
+      fails+=("$runname:$i_map")
+    fi
+  done
 done
 
 [ ! "${fails[*]}" ]
